104caff9e64756d7445e3b7bdf8fcfc233826868
23朵毒蘑菇 <62051421+wurencaideli@users.noreply.github.com>
2026-04-15 17:11:55 +0800

    Add files via upload

--- dist/real-json-data.d.ts
+++ dist/real-json-data.d.ts
@@ -1,4 +1,8 @@
 import { SerialQueue } from './common.js';
+type Option = {
+    cache?: boolean;
+    idKey?: string;
+};
 /**
  * 简单的json数据库，数据必须以数组存在，适用于小量数据
  * 实时操作，每次读取全量数据
@@ -6,9 +10,15 @@ import { SerialQueue } from './common.js';
 export declare class RealJsonData {
     #private;
     jsonPath: string;
-    config: any;
+    keyConfig: any;
+    cache: boolean;
+    idKey: string;
     queueInstance: SerialQueue;
-    constructor(jsonPath: string, config: any);
+    constructor(jsonPath: string, keyConfig: any, option?: Option);
+    /** 预热缓存 */
+    setupCache(): Promise<unknown>;
+    /** 获取缓存数据，方便查找 */
+    getCacheData(): any;
     /** 格式化数据 */
     format(): Promise<unknown>;
     /** 返回所有数据列表 */
@@ -26,3 +36,4 @@ export declare class RealJsonData {
     /** 删除个体，参数是实例 || [实例] */
     delete(instance: any): Promise<unknown>;
 }
+export {};
--- src/real-json-data.ts
+++ src/real-json-data.ts
@@ -3,19 +3,30 @@ import { promises as fs } from 'fs';
 
 import { completionData, SerialQueue } from './common.js';
 
+type Option = {
+    cache?: boolean;
+    idKey?: string;
+};
 /**
  * 简单的json数据库，数据必须以数组存在，适用于小量数据
  * 实时操作，每次读取全量数据
  */
 export class RealJsonData {
     jsonPath = '';
-    config: any = {};
+    keyConfig: any = {};
+    cache: boolean = false;
+    idKey: string = 'id__local__';
     queueInstance: SerialQueue;
-    constructor(jsonPath: string, config: any) {
+    #cacheData: any = [];
+    constructor(jsonPath: string, keyConfig: any, option: Option = {}) {
+        this.cache = !!option.cache;
+        if (option.idKey) {
+            this.idKey = option.idKey;
+        }
         this.queueInstance = new SerialQueue();
         this.jsonPath = jsonPath;
-        this.config = Object.assign({}, config);
-        this.config['id__local__'] = {
+        this.keyConfig = Object.assign({}, keyConfig);
+        this.keyConfig[this.idKey] = {
             default: () => {
                 return uuidv4();
             },
@@ -28,12 +39,36 @@ export class RealJsonData {
             throw 'data is not an array json string.';
         }
         list = list.map((item) => {
-            return completionData(item, this.config);
+            return completionData(item, this.keyConfig);
         });
+        if (this.cache) {
+            this.#cacheData = list;
+        }
         return list;
     }
     async #write(data: any) {
-        return fs.writeFile(this.jsonPath, JSON.stringify(data, null, 2), 'utf8');
+        const content = JSON.stringify(data, null, 2);
+        await fs.writeFile(this.jsonPath, content, 'utf8');
+        if (this.cache) {
+            this.#cacheData = JSON.parse(content);
+        }
+        return;
+    }
+    /** 预热缓存 */
+    async setupCache() {
+        if (!this.cache) {
+            throw 'No cache configured';
+        }
+        return await this.queueInstance.push(async () => {
+            return await this.#read();
+        });
+    }
+    /** 获取缓存数据，方便查找 */
+    getCacheData() {
+        if (!this.cache) {
+            throw 'No cache configured';
+        }
+        return this.#cacheData;
     }
     /** 格式化数据 */
     async format() {
@@ -72,7 +107,7 @@ export class RealJsonData {
         return await this.queueInstance.push(async () => {
             await this.#read(); // 读取检测
             list = list.map((item: any) => {
-                return completionData(item, this.config);
+                return completionData(item, this.keyConfig);
             });
             await this.#write(list);
             return list;
@@ -82,10 +117,10 @@ export class RealJsonData {
     async add(data: any) {
         return await this.queueInstance.push(async () => {
             const list = await this.#read();
-            if (list.find((item: any) => item.id__local__ === data.id__local__)) {
+            if (list.find((item: any) => item[this.idKey] === data[this.idKey])) {
                 throw 'ERROR: Repeating Instances';
             }
-            data = completionData(data, this.config);
+            data = completionData(data, this.keyConfig);
             list.push(data);
             await this.#write(list);
             return data;
@@ -95,13 +130,13 @@ export class RealJsonData {
     async update(instance: any, data: any) {
         return await this.queueInstance.push(async () => {
             const list = await this.#read();
-            const target = list.find((item: any) => item.id__local__ == instance.id__local__);
+            const target = list.find((item: any) => item[this.idKey] == instance[this.idKey]);
             if (!target) {
                 throw 'ERROR: No corresponding instance found';
             }
             Object.keys(target).forEach((key) => {
-                if (key === 'id__local__') return;
-                if (!this.config.hasOwnProperty(key)) return;
+                if (key === this.idKey) return;
+                if (!this.keyConfig.hasOwnProperty(key)) return;
                 if (data.hasOwnProperty(key) && data[key] !== undefined) {
                     target[key] = data[key];
                 }
@@ -119,10 +154,10 @@ export class RealJsonData {
             }
             const signMap: any = {};
             instance.forEach((item: any) => {
-                signMap[item.id__local__] = true;
+                signMap[item[this.idKey]] = true;
             });
             const newList = list.filter((item: any) => {
-                return !signMap[item.id__local__];
+                return !signMap[item[this.idKey]];
             });
             await this.#write(newList);
             return newList;
